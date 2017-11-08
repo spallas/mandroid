dd3455611ce9abcea6914fff395f7c224b587911
Valerio <colitta.valerio@gmail.com>
2017-11-08 15:55:10 +0100

    Added dataset vectorization.

--- mandroid/dataset_preprocessing.py
+++ mandroid/dataset_preprocessing.py
@@ -55,14 +55,20 @@ def load_dataset(dataset_path):
     Loads data from Drebin dataset and stores the information of each file in a
     dictionary with the file data + the pair name: file_name
 
-    :param dataset_path: path of the Drebin dataset
+    :param dataset_path: path of the drebin dataset
     :return: list of dictionaries containing files data.
     """
     data = []
+    hash = []
+    i=0
     for file in os.listdir(dataset_path):
+        if i >= 200:
+            break
         # load file data
         data.append(parse_file(dataset_path, file))
-    return data
+        hash.append(file)
+        i = i+1
+    return data, hash
 
 
 def parse_file(dataset_path, file_name):
@@ -85,54 +91,10 @@ def parse_file(dataset_path, file_name):
     :return: dictionary of features.
     """
     file_dict = {}
-
-    with open(dataset_path+file_name, "r") as f:
+    with open(dataset_path + "\\" + file_name, "r") as f:
         for line in f:
-            info = line.strip().split("::")
-            if info[0] == "feature":
-                if "req_hw" in file_dict:
-                    file_dict["req_hw"].append(info[1])
-                else:
-                    file_dict["req_hw"] = [info[1]]
-            elif info[0] == "permission":
-                if "req_perm" in file_dict:
-                    file_dict["req_perm"].append(info[1])
-                else:
-                    file_dict["req_perm"] = [info[1]]
-            elif (info[0] == "activity"
-                  or info[0] == "service_receiver"
-                  or info[0] == "provider"
-                  or info[0] == "service"):
-                if "app_comp" in file_dict:
-                    file_dict["app_comp"].append(info[1])
-                else:
-                    file_dict["app_comp"] = [info[1]]
-            elif info[0] == "intent":
-                if "filt_int" in file_dict:
-                    file_dict["filt_int"].append(info[1])
-                else:
-                    file_dict["filt_int"] = [info[1]]
-            elif info[0] == "api_call":
-                if "rstr_api" in file_dict:
-                    file_dict["rstr_api"].append(info[1])
-                else:
-                    file_dict["rstr_api"] = [info[1]]
-            elif info[0] == "real_permission":
-                if "use_perm" in file_dict:
-                    file_dict["use_perm"].append(info[1])
-                else:
-                    file_dict["use_perm"] = [info[1]]
-            elif info[0] == "call":
-                if "susp_api" in file_dict:
-                    file_dict["susp_api"].append(info[1])
-                else:
-                    file_dict["susp_api"] = [info[1]]
-            elif info[0] == "url":
-                if "use_urls" in file_dict:
-                    file_dict["use_urls"].append(info[1])
-                else:
-                    file_dict["use_urls"] = [info[1]]
-
+            file_dict[line.strip()] = True
+            # info = line.strip().split("::")
     return file_dict
 
 
