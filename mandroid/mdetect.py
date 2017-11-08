e8d2b99cc4e93b8418cd3c764ba079e4a04a9487
spallas <davide.spallaccini@gmail.com>
2017-11-08 17:25:03 +0100

    Add detection training.

--- mandroid/mdetect.py
+++ mandroid/mdetect.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 
+from sklearn.svm import LinearSVC
+
 """
     This module is able to check is a given application is an Android malware.
 """
 
+
+def train_and_validate(X, y):
+    """
+    Train the model with X, y from dataset and evaluate performance with
+    10-fold cross validation. Print vali
+    :param X:
+    :param y:
+    :return:
+    """
+    clf = LinearSVC(random_state=0)
+    clf.fit(X, y)
+    return
